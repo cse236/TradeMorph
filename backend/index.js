06d66844c1949051586a9954ee94a04b5add6022
omkar shirsath <omkarshirsath28@gmail.com>
2025-09-01 15:55:53 +0530

    feat: implement /api/upload route - Added multer for CSV uploads - Parse CSV and insert trades into MongoDB - Ensure uploads directory exists - Auto-delete temp files after processing

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db")
 const mongoose = require("mongoose");
+const uploadRoutes = require("./routes/uploadRoutes");
 dotenv.config();
 
 connectDB();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/upload",uploadRoutes);
 app.get("/",(req,res)=>{
     res.json({message : "working api call ..."});
 })
---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const csv = require("csv-parser");
+const Trade = require("../models/Trade");
+
+const router = express.Router();
+
+//Ensure uploads directory exists
+const UPLOAD_DIR = path.join(__dirname,"..","uploads");
+fs.mkdirSync(UPLOAD_DIR,{recursive:true});
+
+//Multer config
+const upload = multer({dest:UPLOAD_DIR});
+
+//post /api/upload
+
+router.post("/",upload.single("file"),(req,res)=>{
+    if(!req.file)
+        return res.status(400).json({error:"NO file uploaded(field name must be 'file')."});
+
+    const results =[];
+    fs.createReadStream(req.file.path)
+    .pipe(csv())
+    .on("data",(row)=>{
+        results.push({
+            symbol:row.symbol,
+            qty:Number(row.qty),
+            buyPrice:Number(row.buy),
+            sellPrice:Number(row.sell),
+            entryDate:new Date(row.entryDate),
+            exitDate:new Date(row.exitDate),
+        });
+    })
+    .on("end",async()=>{
+    try{
+        await Trade.insertMany(results);
+        res.json({message:"Trades uploaded successfully",count:results.length});
+
+    }catch(err){
+        res.status(500).json({error:err.message});
+    }finally{
+        fs.unlink(req.file.path, ()=>{});//cleanup the temp file
+
+    }
+ });
+});
+
+
+module.exports = router;
+
